6abfc36710eafeecd23324cc059eb26acb132ab8
uiday009u <59877397+uiday009u@users.noreply.github.com>
2024-04-07 00:21:38 +0530

    Create OrderDTO.java

---
+++ OrderDTO.java
@@ -0,0 +1,31 @@
+public class OrderDTO {
+
+    private Long id;
+    private Long productId;
+    private int quantity;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
