7dbe0cb84e3eaa0341801910b1b57325631fbf4e
uiday009u <59877397+uiday009u@users.noreply.github.com>
2024-04-07 00:17:44 +0530

    Create ProductOrderManagementApplication.java

---
+++ ProductOrderManagementApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class ProductOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderManagementApplication.class, args);
+    }
+}
