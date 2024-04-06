fdaa32516e19b4a149f662037c20b81f1eac1a89
uiday009u <59877397+uiday009u@users.noreply.github.com>
2024-04-07 00:22:05 +0530

    Create OrderProcessingServiceApplication.java

---
+++ OrderProcessingServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class OrderProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessingServiceApplication.class, args);
+    }
+}
