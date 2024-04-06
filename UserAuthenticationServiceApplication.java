df9be86e33337b75c8fec044afedc4b5836f381f
uiday009u <59877397+uiday009u@users.noreply.github.com>
2024-04-07 00:12:59 +0530

    Create UserAuthenticationServiceApplication.java

---
+++ UserAuthenticationServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class UserAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationServiceApplication.class, args);
+    }
+}
